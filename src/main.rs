87f262d4762df3c7c086719d70259debccc7c6e7
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-07 12:22:04 +0000

    Add basic code for reading all lines of misspelled_words.txt and printing them

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,25 @@
 //             print word
 //             break
 
-fn main() {
-    println!("Hello, world!");
+mod user_input;
+
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let directory: String = user_input::get_user_input("Enter directory to search:");
+
+    let file: File = File::open("misspelled_words.txt")?;
+    let reader: BufReader<File> = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line: String = line.unwrap();
+        println!("{}", line);
+    }
+
+    println!("Directory: {}", directory); // so the compiler doesn't complain
+
+    Ok(())
 }
 
 /*
