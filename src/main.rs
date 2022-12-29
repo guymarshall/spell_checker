94264ea47567c5f6bbb190c8ccaf1dba0b2f2251
Guy Marshall <guymarshall9@gmail.com>
2022-12-29 22:08:02 +0000

    Add code for reading data recursively and saving to vector

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,13 @@
 #![forbid(unsafe_code)]
 
-// for each word in misspelled_words.txt, search whole directory. As soon as word is found, break the loop and print the word, then move to the next word until done
-// for word in list
-//     for file in directory
-//         if word found
-//             print word
-//             break
-
-// mod user_input;
-
-// use std::fs::File;
-// use std::io::{self, prelude::*, BufReader};
-
-// fn main() -> io::Result<()> {
-//     let directory: String = user_input::get_user_input("Enter directory to search:");
-
-//     let file: File = File::open("misspelled_words.txt")?;
-//     let reader: BufReader<File> = BufReader::new(file);
-
-//     for line in reader.lines() {
-//         let line: String = line.unwrap();
-//         println!("{}", line);
-//     }
-
-//     println!("Directory: {}", directory); // so the compiler doesn't complain
-
-//     Ok(())
-
-// }
-
 mod user_input;
 
 use std::fs::{self, File, DirEntry};
 use std::path::{Path, PathBuf};
 use std::io::{prelude::*, BufReader};
 
-fn read_files(directory: &Path) {
+fn read_files(directory: &Path) -> Vec<String> {
+    let mut contents_vector: Vec<String> = Vec::new();
     if directory.is_dir() {
         for entry in fs::read_dir(directory).unwrap() {
             let entry: DirEntry = entry.unwrap();
@@ -43,11 +15,13 @@ fn read_files(directory: &Path) {
             if path.is_dir() {
                 read_files(&path);
             } else {
-                let contents: String = fs::read_to_string(path).unwrap();
-                println!("{}", contents);
+                let contents: String = fs::read_to_string(path).unwrap_or("".to_string());
+                println!("{}", &contents);
+                contents_vector.push(contents);
             }
         }
     }
+    contents_vector
 }
 
 fn main() {
@@ -64,5 +38,7 @@ fn main() {
     let path: String = user_input::get_user_input("Enter path to search:");
 
     let directory: &Path = Path::new(&path);
-    read_files(directory);
+    let contents_vector: Vec<String> = read_files(directory);
+
+    println!("{:#?}", contents_vector);
 }
