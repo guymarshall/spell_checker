21a34867dcb31221b7b155885170ba515c5e3f13
Guy Marshall <guymarshall9@gmail.com>
2022-12-29 21:19:45 +0000

    Add code for reading files recursively

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,63 @@
 //             print word
 //             break
 
+// mod user_input;
+
+// use std::fs::File;
+// use std::io::{self, prelude::*, BufReader};
+
+// fn main() -> io::Result<()> {
+//     let directory: String = user_input::get_user_input("Enter directory to search:");
+
+//     let file: File = File::open("misspelled_words.txt")?;
+//     let reader: BufReader<File> = BufReader::new(file);
+
+//     for line in reader.lines() {
+//         let line: String = line.unwrap();
+//         println!("{}", line);
+//     }
+
+//     println!("Directory: {}", directory); // so the compiler doesn't complain
+
+//     Ok(())
+
+// }
+
 mod user_input;
 
-use std::fs::File;
+use std::fs::{self, File, DirEntry};
+use std::path::{Path, PathBuf};
 use std::io::{self, prelude::*, BufReader};
 
-fn main() -> io::Result<()> {
-    let directory: String = user_input::get_user_input("Enter directory to search:");
-
-    let file: File = File::open("misspelled_words.txt")?;
-    let reader: BufReader<File> = BufReader::new(file);
-
-    for line in reader.lines() {
-        let line: String = line.unwrap();
-        println!("{}", line);
-    }
-
-    println!("Directory: {}", directory); // so the compiler doesn't complain
-
-    Ok(())
-
-    /*
-    use std::fs;
-    use std::path::Path;
-
-    fn read_files(dir: &Path) {
-        if dir.is_dir() {
-            for entry in fs::read_dir(dir).unwrap() {
-                let entry = entry.unwrap();
-                let path = entry.path();
-                if path.is_dir() {
-                    read_files(&path);
-                } else {
-                    let contents = fs::read_to_string(path).unwrap();
-                    println!("{}", contents);
-                }
+fn read_files(directory: &Path) {
+    if directory.is_dir() {
+        for entry in fs::read_dir(directory).unwrap() {
+            let entry: DirEntry = entry.unwrap();
+            let path: PathBuf = entry.path();
+            if path.is_dir() {
+                read_files(&path);
+            } else {
+                let contents: String = fs::read_to_string(path).unwrap();
+                println!("{}", contents);
             }
         }
     }
+}
 
-    fn main() {
-        let dir = Path::new("/path/to/directory");
-        read_files(dir);
+fn main() {
+    
+    let file: File = File::open("misspelled_words.txt").expect("Invalid path.");
+    let reader: BufReader<File> = BufReader::new(file);
+    
+    let mut misspelled_words: Vec<String> = Vec::new();
+    for line in reader.lines() {
+        misspelled_words.push(line.unwrap());
     }
-    */
+    for misspelled_word in misspelled_words {
+        println!("{}", misspelled_word);
+    }
+    let path: String = user_input::get_user_input("Enter path to search:");
+
+    let directory: &Path = Path::new(&path);
+    read_files(directory);
 }
