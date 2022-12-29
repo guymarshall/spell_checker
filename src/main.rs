d641706412217829f6bce298af121765767f2f5c
Guy Marshall <guymarshall9@gmail.com>
2022-12-29 23:47:47 +0000

    Add print statement at start to give example of format for path

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use std::io::{prelude::*, BufReader};
 use scan_directory::save_file_contents;
 
 fn main() {
+    println!("Example path: 'c:/users/YOUR_USERNAME/downloads'");
     let file: File = File::open("misspelled_words.txt").expect("Invalid path.");
     let reader: BufReader<File> = BufReader::new(file);
     
@@ -35,7 +36,7 @@ fn main() {
     // });
     for misspelled_word in &misspelled_words {
         for (_, value) in &path_contents {
-            if value.contains(misspelled_word) {
+            if value.contains(misspelled_word) && !found_words.contains(misspelled_word) {
                 found_words.push(misspelled_word.to_string());
             }
         }
