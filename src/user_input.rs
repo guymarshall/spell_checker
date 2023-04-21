780bc8f565d49fb9a05a41b2d9b7b4308ab9e1de
Guy Marshall <guymarshall9@gmail.com>
2023-04-21 18:26:39 +0100

    Revert to old user_input.rs with warning comment

--- src/user_input.rs
+++ src/user_input.rs
@@ -1,33 +1,17 @@
 #![forbid(unsafe_code)]
 
-use std::io;
-use std::error::Error;
+/*
+    WARNING: STILL USING OLD user_input.rs
+ */
 
-pub fn get_user_input(prompt: &str) -> Result<String, Box<dyn Error>> {
+use std::io;
+
+pub fn get_user_input(prompt: &str) -> String {
     println!("{}", prompt);
 
     let mut user_input: String = String::new();
 
-    io::stdin().read_line(&mut user_input)?;
+    io::stdin().read_line(&mut user_input).expect("Failed to read line");
 
-    let number: String = user_input.trim().parse().map_err(|_| "Please enter a valid integer!")?;
-
-    if number <= 0 {
-        return Err(From::from("The number must be greater than 0!"));
-    }
-
-    Ok(number)
-}
-
-pub fn input(prompt: &str) -> String {
-    loop {
-        match get_user_input(prompt) {
-            Ok(count) => {
-                return count;
-            },
-            Err(error) => {
-                println!("Error: {}", error);
-            },
-        };
-    }
+    user_input.trim().to_string()
 }
