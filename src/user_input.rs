690253ae08ed017a27a975245750aba454797957
Guy Marshall <guymarshall9@gmail.com>
2023-04-21 19:10:01 +0100

    Use new user_input.rs

--- src/user_input.rs
+++ src/user_input.rs
@@ -1,17 +1,27 @@
 #![forbid(unsafe_code)]
 
-/*
-    WARNING: STILL USING OLD user_input.rs
- */
-
 use std::io;
+use std::error::Error;
 
-pub fn get_user_input(prompt: &str) -> String {
+pub fn get_user_input(prompt: &str) -> Result<String, Box<dyn Error>> {
     println!("{}", prompt);
 
     let mut user_input: String = String::new();
 
-    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    io::stdin().read_line(&mut user_input)?;
 
-    user_input.trim().to_string()
+    Ok(user_input)
+}
+
+pub fn input(prompt: &str) -> String {
+    loop {
+        match get_user_input(prompt) {
+            Ok(input) => {
+                return input;
+            },
+            Err(error) => {
+                println!("Error: {}", error);
+            },
+        };
+    }
 }
