2cdc2b488fa10bfa4169d4b871b72bd1a87f5505
Guy Marshall <guymarshall9@gmail.com>
2025-05-10 19:26:30 +0100

    UPDATE: Save results to output.txt

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import argparse
 
 MISSPELLED_WORDS_FILENAME: str = 'misspelled_words.txt'
+OUTPUT_FILENAME: str = 'output.txt'
 
 def save_file_contents(directory: Path) -> dict[str, str]:
     path_contents: dict[str, str] = {}
@@ -17,7 +18,6 @@ def save_file_contents(directory: Path) -> dict[str, str]:
                         contents = file.read()
                 except Exception:
                     contents = ''
-
                 path_contents[str(entry)] = contents
     return path_contents
 
@@ -27,19 +27,20 @@ def main():
     arguments = argument_parser.parse_args()
     path: str = arguments.path
 
-    try:
-        directory: Path = Path(path)
-        print(f'Scanning directory "{directory}"')
-    except TypeError:
+    if not path:
         print('Path must be given via --path flag')
         exit(1)
+
+    directory: Path = Path(path)
+    print(f'Scanning directory "{directory}"')
+
     path_contents: dict[str, str] = save_file_contents(directory)
 
     try:
         with open(MISSPELLED_WORDS_FILENAME, 'r', encoding='utf-8') as file:
             misspelled_words = [line.strip() for line in file.readlines()]
     except FileNotFoundError:
-        print('Invalid path.')
+        print(f'File "{MISSPELLED_WORDS_FILENAME}" not found.')
         return
 
     matched_words_by_file = {
@@ -48,8 +49,9 @@ def main():
         if any(word in contents for word in misspelled_words)
     }
 
-    for path, words in matched_words_by_file.items():
-        print(f'{path}: {', '.join(words)}')
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as output_file:
+        for path, words in matched_words_by_file.items():
+            output_file.write(f'{path}: {words}\n')
 
 if __name__ == '__main__':
     main()
